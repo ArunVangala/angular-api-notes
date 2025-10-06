f811fa684ce95d96a3a2f65105c166a090079548
ArunVangala <vangalaarun74@gmail.com>
2025-10-06 18:13:51 +0530

    Update ConnectionStringService.cs

--- Services/ConnectionStringService.cs
+++ Services/ConnectionStringService.cs
@@ -15,29 +15,8 @@ namespace DatabaseAPI.Services
         {
             return new Dictionary<string, ServerCredentials>(StringComparer.OrdinalIgnoreCase)
             {
-                // SAMAST Servers
-                ["DB1"] = new("172.16.13.11", "sa", "TsDs!1$"),
-                ["DB2"] = new("172.16.13.12", "sa", "Tvd@123"),
-                ["DB3"] = new("172.16.11.12", "sa", "Smst@7h64"),
-                
-                // GTD Servers
-                ["VM4"] = new("10.10.204.161", "sa", "hoeur#q8"),
-                ["VM5"] = new("10.10.204.162", "sa", "Gtdtvd@2"),
-                ["VM6"] = new("10.10.204.163", "sa", "Gtdtvd@3"),
-                ["VM7"] = new("10.10.204.164", "sa", "juS$$qq62"),
-                ["VM8"] = new("10.10.204.165", "sa", "Ywz^&mr0"),
-                ["VM9"] = new("10.10.204.166", "sa", "O0tc%lvX"),
-                
-                // G2G Servers
-                ["DB5"] = new("45.114.246.176", "sa", "CpWQ6#uDntM7f4c$"),
-                
-                // Dev Servers
-                ["DEV1"] = new("192.168.0.101", "sa", "Tvd1234!"),
-                ["DEV2"] = new("43.243.79.150", "sa", "hoeur#q8"),
-                ["DEV3"] = new("192.168.0.103", "sa", "Dev3Pass!"),
-                ["DEV4"] = new("192.168.0.104", "sa", "Dev4Pass!"),
-                ["DEV5"] = new("192.168.0.105", "sa", "Dev5Pass!"),
-                ["DEV6"] = new("192.168.0.106", "sa", "Dev6Pass!")
+                //  Servers  names and passwords
+               
             };
         }
 
